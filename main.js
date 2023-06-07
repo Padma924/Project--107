4da7ba32ba1a7705fe8fbf4de97fba2f63e213d2
Padma924 <115601128+Padma924@users.noreply.github.com>
2023-06-06 17:28:27 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function StartClassification()
+{
+navigator.mediaDevices.getUserMedia({audio: true});
+classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/g4rJVrADn/model.json', modelReady);
+}
+function modelReady(){
+clssifier.classify(gotResults);
+}
